11bc0957ac92b97174bf67c4dd6e28d14b142e08
mrizkyprtm <m.rizkypratama319@gmail.com>
2025-02-01 20:32:56 +0700

    slicing navbar and hero section

---
+++ src/App.vue
@@ -0,0 +1,150 @@
+<script setup>
+import NavBar from '@/components/NavBar.vue'
+</script>
+
+<template>
+  <NavBar />
+  <main class="max-w-7xl mx-auto">
+    <section id="hero" class="mt-15">
+      <div class="flex items-center">
+        <div class="flex-1 flex flex-col gap-10">
+          <div
+            id="hero-badge"
+            class="flex items-center bg-white w-fit py-2 px-4 rounded-full gap-2"
+          >
+            <svg
+              width="25"
+              height="25"
+              viewBox="0 0 25 25"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M12.5 22.5C18.0228 22.5 22.5 18.0228 22.5 12.5C22.5 6.97715 18.0228 2.5 12.5 2.5C6.97715 2.5 2.5 6.97715 2.5 12.5C2.5 18.0228 6.97715 22.5 12.5 22.5Z"
+                stroke="#080C2E"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M8.50001 3.5H9.50001C7.55001 9.34 7.55001 15.66 9.50001 21.5H8.50001"
+                stroke="#080C2E"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M15.5 3.5C17.45 9.34 17.45 15.66 15.5 21.5"
+                stroke="#080C2E"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M3.5 16.5V15.5C9.34 17.45 15.66 17.45 21.5 15.5V16.5"
+                stroke="#080C2E"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M3.5 9.50001C9.34 7.55001 15.66 7.55001 21.5 9.50001"
+                stroke="#080C2E"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+            <p class="font-semibold">We host more than 120,000 websites</p>
+          </div>
+          <div id="hero-text">
+            <h1 class="text-[70px] font-bold">
+              Grow Online <br class="hidden md:block" />
+              Business Faster.
+            </h1>
+            <p class="text-[#5D5D7C]">
+              We provide a variety of servers to grow your users <br class="hidden md:block" />
+              acquisition much user-friendly and boosting up sales.
+            </p>
+          </div>
+          <div id="hero-btn" class="flex items-center">
+            <button class="px-10 py-4 bg-[#640EF1] rounded-full text-white font-bold">
+              Try Free Trial
+            </button>
+            <button class="px-10 py-4 font-semibold text-[#640EF1] flex items-center gap-1">
+              <svg
+                width="25"
+                height="24"
+                viewBox="0 0 25 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M20.5 12.2V13.9C20.5 17.05 18.7 18.4 16 18.4H7C4.3 18.4 2.5 17.05 2.5 13.9V8.5C2.5 5.35 4.3 4 7 4H9.7C9.57 4.38 9.5 4.8 9.5 5.25V9.15002C9.5 10.12 9.82 10.94 10.39 11.51C10.96 12.08 11.78 12.4 12.75 12.4V13.79C12.75 14.3 13.33 14.61 13.76 14.33L16.65 12.4H19.25C19.7 12.4 20.12 12.33 20.5 12.2Z"
+                  stroke="#640EF1"
+                  stroke-width="2"
+                  stroke-miterlimit="10"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M22.5 5.25V9.15002C22.5 10.64 21.74 11.76 20.5 12.2C20.12 12.33 19.7 12.4 19.25 12.4H16.65L13.76 14.33C13.33 14.61 12.75 14.3 12.75 13.79V12.4C11.78 12.4 10.96 12.08 10.39 11.51C9.82 10.94 9.5 10.12 9.5 9.15002V5.25C9.5 4.8 9.57 4.38 9.7 4C10.14 2.76 11.26 2 12.75 2H19.25C21.2 2 22.5 3.3 22.5 5.25Z"
+                  stroke="#640EF1"
+                  stroke-width="2"
+                  stroke-miterlimit="10"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M7.89999 22H15.1"
+                  stroke="#640EF1"
+                  stroke-width="2"
+                  stroke-miterlimit="10"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M11.5 18.4V22"
+                  stroke="#640EF1"
+                  stroke-width="2"
+                  stroke-miterlimit="10"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M18.9955 7.25H19.0045"
+                  stroke="#640EF1"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M16.1957 7.25H16.2047"
+                  stroke="#640EF1"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M13.3954 7.25H13.4044"
+                  stroke="#640EF1"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+              <span> Schedule a demo </span>
+            </button>
+          </div>
+        </div>
+        <div class="hero-img w-[500px]">
+          <img class="w-full" src="/hero1.png" alt="" />
+        </div>
+      </div>
+    </section>
+  </main>
+
+  <footer></footer>
+</template>
+
+<style scoped></style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,44 @@
+<script setup>
+import Logo from '@/components/icon/Logo.vue'
+import Cart from '@/components/icon/Cart.vue'
+
+const navLinks = [
+  {
+    name: 'Products',
+    link: '#',
+  },
+  {
+    name: 'Services',
+    link: '#',
+  },
+  {
+    name: 'Pricing',
+    link: '#',
+  },
+  {
+    name: 'Testimonials',
+    link: '#',
+  },
+  {
+    name: 'Blog',
+    link: '#',
+  },
+]
+</script>
+
+<template>
+  <nav class="flex items-center justify-between max-w-7xl mx-auto py-10">
+    <Logo />
+    <ul class="flex items-center gap-x-7">
+      <li v-for="link in navLinks" :key="link.name">{{ link.name }}</li>
+    </ul>
+    <div id="button" class="flex items-center gap-2">
+      <button class="bg-white p-2 rounded-full">
+        <Cart />
+      </button>
+      <a href="#" class="rounded-full px-7 py-2 bg-[#080C2E] text-white font-semibold text-nowrap"
+        >Sign In</a
+      >
+    </div>
+  </nav>
+</template>
